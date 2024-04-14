789a7946b21cb57cdcdb2f35ac319fb7008e40c2
prolisbet <lizatrushina96@gmail.com>
2024-04-15 01:07:29 +0300

    add score widget

--- main.py
+++ main.py
@@ -20,9 +20,23 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+score = 0
+font = pygame.font.Font(None, 24)
+background_color = (255, 255, 255)
+
 running = True
 while running:
     screen.fill(color)
+
+    text_background_width = 120
+    text_background_height = 25
+    text_background_x = 10
+    text_background_y = 10
+    text_background = pygame.Rect(text_background_x, text_background_y, text_background_width, text_background_height)
+    pygame.draw.rect(screen, background_color, text_background)
+
+    score_surface = font.render(f'Очки: {score}', True, (0, 0, 0))
+    screen.blit(score_surface, [15, 15])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -31,6 +45,7 @@ while running:
             if (
                     target_x < mouse_x < target_x + target_width) and (
                     target_y < mouse_y < target_y + target_height):
+                score += 10
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
     screen.blit(target_img, (target_x, target_y))
